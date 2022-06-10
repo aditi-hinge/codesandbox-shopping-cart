24b317b3568d2c45efa746f15226e6c83296ce5b
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-06-10 16:30:39 +0900

    responsive category item

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   flex: 1;
@@ -6,11 +7,14 @@ const Container = styled.div`
   height: 85vh;
   position: relative;
 `;
+
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  ${mobile({ height: "30vh" })}
 `;
+
 const Info = styled.div`
   position: absolute;
   height: 100%;
