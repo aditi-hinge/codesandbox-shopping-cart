d294be39d3db81db3a388cad1f2fd41772e755d4
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-06-11 16:33:42 +0900

    responsive shoplinks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import CompanyInfo from "./CompanyInfo";
 import ShopLinks from "./ShopLinks";
 
+// import { mobile } from "../responsive";
+
 const Container = styled.div`
   margin: 1em;
 `;
