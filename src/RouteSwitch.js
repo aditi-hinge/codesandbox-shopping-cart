50660fffbb0aad8a7af53b91289606868c806a91
Aditi <95167364+aditi-hinge@users.noreply.github.com>
2022-06-12 20:09:09 +0900

    added path to individual pages

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import App from "./App";
-// import ProductList from "./pages/ProductList";
-// import SingleProduct from "./pages/SingleProduct";
-// import Register from "./pages/Register";
+import App from "./App";
+import ProductList from "./pages/ProductList";
+import SingleProduct from "./pages/SingleProduct";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
-// import Cart from "./pages/Cart";
+import Cart from "./pages/Cart";
 
 export default function RouteSwitch() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<App />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/singleproduct" element={<SingleProduct />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
